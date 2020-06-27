8561774b15e921c8b254a47a70d9e573ecf41dcf
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2020-06-27 13:40:44 +0200

    Implement basic result serializer

--- src/route_scheduler/serializers.py
+++ src/route_scheduler/serializers.py
@@ -1,4 +1,5 @@
 from marshmallow import Schema, fields
+from route_scheduler.types import Route
 
 coordinate = fields.Tuple((fields.Integer, fields.Integer))
 
@@ -9,8 +10,21 @@ class TaskSchema(Schema):
     coordinates = fields.List(coordinate)
 
 
+class ResultSchema(Schema):
+    visiting_order = fields.List(fields.Integer())
+    objective = fields.Integer()
+    length = fields.Integer()
+
+
 task_schema = TaskSchema()
+result_schema = ResultSchema()
 
 
 def deserialize_task(raw_json: bytes):
     return task_schema.loads(raw_json.decode())
+
+
+def serialize_result(result: Route) -> str:
+    v_order, obj, length = result
+    result = {"visiting_order": v_order, "objective": obj, "length": length}
+    return result_schema.dumps(result)
--- src/route_scheduler/tests/test_serializers.py
+++ src/route_scheduler/tests/test_serializers.py
@@ -1,4 +1,4 @@
-from route_scheduler.serializers import deserialize_task
+from route_scheduler.serializers import deserialize_task, serialize_result
 
 
 test_msg = b'{"coordinates":[[1,0],[0,1]], "start":0, "cars":1}'
@@ -7,3 +7,11 @@ expected = {"coordinates": [(1, 0), (0, 1)], "start": 0, "cars": 1}
 
 def test_deserialize_task_basic():
     assert deserialize_task(test_msg) == expected
+
+
+expected_res = '{"visiting_order": [0, 1, 2, 0], "length": 200, "objective": 200}'
+
+
+def test_serialize_result_basic():
+    route = ([0, 1, 2, 0], 200, 200)
+    assert serialize_result(route) == expected_res
