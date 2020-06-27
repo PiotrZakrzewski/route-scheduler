f6cff57f0e562c4345f56dc5987439bdb609793b
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2020-06-27 13:03:03 +0200

    Add type hint annotations

---
+++ src/route_scheduler/types.py
@@ -0,0 +1,7 @@
+from typing import Tuple, Sequence
+
+
+Coordinate = Tuple[int, int]
+Coordinates = Sequence[Coordinate]
+Route = Tuple[Sequence[int], int, int]
+DistMatrix = Sequence[Sequence[int]]
