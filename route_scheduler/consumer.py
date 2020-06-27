a37f05276d5ae6d55e949feb9fabb3b2483a75d0
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2020-06-27 17:05:07 +0200

    Retry connecting to the MQ server

--- route_scheduler/consumer.py
+++ route_scheduler/consumer.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import time
 import pika
 from route_scheduler.serializers import deserialize_task, serialize_result
 from route_scheduler.scheduler import find_routes
@@ -7,16 +8,32 @@ from marshmallow.exceptions import MarshmallowError
 
 log = logging.getLogger("consumer")
 
+RETRY_COOLOFF = 3
 
-def start_consuming():
+
+def connect_with_retry():
     mq_usr = os.getenv("MQ_USERNAME", "user")
     mq_pwd = os.getenv("MQ_PWD", "bitnami")
     mq_host = os.getenv("MQ_HOST", "localhost")
     mq_port = int(os.getenv("MQ_PORT", "5672"))
     credentials = pika.PlainCredentials(mq_usr, mq_pwd)
     params = pika.ConnectionParameters(mq_host, mq_port, credentials=credentials)
-    connection = pika.BlockingConnection(params)
+    while True:
+        try:
+            connection = pika.BlockingConnection(params)
+        except pika.exceptions.AMQPConnectionError:
+            log.warn(
+                "Could not connect to RabbitMQ, will retry in {} sec...".format(
+                    RETRY_COOLOFF
+                )
+            )
+            time.sleep(RETRY_COOLOFF)
+            continue
+        return connection
 
+
+def start_consuming():
+    connection = connect_with_retry()
     channel = connection.channel()
 
     def callback(ch, method, properties, body):
